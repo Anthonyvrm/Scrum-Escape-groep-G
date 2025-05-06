f77d15b15e44d57740c0ef094b58186e5eaa50d3
RobbertSchwarze <robbertschwarze2309@gmail.com>
2025-05-06 12:11:21 +0200

    Game en player erbij. Zonder getters en setters

---
+++ src/main/java/classes/Game.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Game {
+
+    Game game;
+    Player player;
+
+    public Game (Game game, Player player) {
+        this.game = game;
+        this.player = player;
+    }
+}
---
+++ src/main/java/classes/Player.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Player {
+
+    private String status;
+    private String name;
+    private String position;
+
+    public Player (String status, String name, String position) {
+        this.status = status;
+        this.name = name;
+        this.position = position;
+    }
+}
