382b407dbb6e8933c3fc24c19dd855c4f3748135
anthony <anthonyvrm@gmail.com>
2025-05-08 12:46:41 +0200

    sql implementatie

---
+++ src/main/java/database/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static Connection connect() {
+        String url = "jdbc:sqlite:src/main/resources/database/database.db";
+        try {
+            Connection conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("Database connection error: " + e.getMessage());
+            return null;
+        }
+    }
+}
