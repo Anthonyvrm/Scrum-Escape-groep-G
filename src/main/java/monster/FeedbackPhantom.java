56674e08525f63a375b9daff8ed7c7624116a541
ReesMehorst <24141720@student.hhs.nl>
2025-05-08 11:44:50 +0200

    Beschrijving voor FeedbackPhantom

--- src/main/java/monster/FeedbackPhantom.java
+++ src/main/java/monster/FeedbackPhantom.java
@@ -3,6 +3,11 @@ package monster;
 import classes.IMonster;
 import classes.Monster;
 
+
+//FeedBack Phantom is een spookachtig wezen dat verschijnt zodra het team feedback ontvangt… maar er niets mee doet.
+//Hij spookt door de kamer met vage opmerkingen, onduidelijke verwachtingen en eindeloze meningen zonder actie.
+//Hoe meer ongebruikte feedback, hoe sterker hij wordt.
+
 public class FeedbackPhantom extends Monster implements IMonster {
     FeedbackPhantom(int damage, int healthPoints){
         super(damage,healthPoints);
