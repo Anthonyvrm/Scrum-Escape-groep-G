4f00a32ed1dacea15e92836488ab4562cbb649b0
ReesMehorst <24141720@student.hhs.nl>
2025-05-10 11:19:17 +0200

    FeedbackPhantom dmg methods. met breaks.

--- src/main/java/monster/FeedbackPhantom.java
+++ src/main/java/monster/FeedbackPhantom.java
@@ -16,11 +16,21 @@ public class FeedbackPhantom implements IMonster {
     public void takeDamage(){
         int randomInt = (int)(Math.random() * 5);
         switch (randomInt) {
-            case 0: System.out.println("Ugh... do I really have to pay attention?");
-            case 1: System.out.println("Such a waste... just like all feedback.");
-            case 2: System.out.println("Ugh... feedback again? Like I care...");
-            case 3: System.out.println("Meh... even when I’m hurt, I’m still doing nothing with feedback.");
-            case 4: System.out.println("Alright, alright... I don’t care... but that hurts.");
+            case 0:
+                System.out.println("Ugh... do I really have to pay attention?");
+                break;
+            case 1:
+                System.out.println("Such a waste... just like all feedback.");
+                break;
+            case 2:
+                System.out.println("Ugh... feedback again? Like I care...");
+                break;
+            case 3:
+                System.out.println("Meh... even when I’m hurt, I’m still doing nothing with feedback.");
+                break;
+            case 4:
+                System.out.println("Alright, alright... I don’t care... but that hurts.");
+                break;
         }
     }
 
@@ -28,11 +38,21 @@ public class FeedbackPhantom implements IMonster {
     public void dealDamage(){
         int randomInt = (int)(Math.random() * 5);
         switch (randomInt) {
-            case 0: System.out.println("Feel that? That's what useless feedback does!");
-            case 1: System.out.println("Painful, isn’t it? Just like unclear expectations.");
-            case 2: System.out.println("Let me destroy your sprint with one badly timed opinion!");
-            case 3: System.out.println("Boom! Now you’re doubting everything you just built.");
-            case 4: System.out.println("I just derailed your progress — you're welcome.");
+            case 0:
+                System.out.println("Feel that? That's what useless feedback does!");
+                break;
+            case 1:
+                System.out.println("Painful, isn’t it? Just like unclear expectations.");
+                break;
+            case 2:
+                System.out.println("Let me destroy your sprint with one badly timed opinion!");
+                break;
+            case 3:
+                System.out.println("Boom! Now you’re doubting everything you just built.");
+                break;
+            case 4:
+                System.out.println("I just derailed your progress — you're welcome.");
+                break;
         }
     }
 }
