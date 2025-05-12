733d7c4199c268b49d580d863837ab69eca606e0
anthonyvrm <anthonyvrm@gmail.com>
2025-05-12 15:50:08 +0200

    database

--- src/main/java/database/DatabaseSetup.java
+++ src/main/java/database/DatabaseSetup.java
@@ -1,4 +1,36 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseSetup {
+
+    public static void createTables() {
+
+        String createSaveTable = """
+    CREATE TABLE IF NOT EXISTS Save (
+        Status INTEGER NOT NULL,
+        Name TEXT UNIQUE PRIMARY KEY NOT NULL,
+        Position INT NOT NULL
+    );
+    """;
+
+
+
+
+
+        try (Connection conn = DatabaseConnection.connect();
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createSaveTable);
+        } catch (SQLException e) {
+            System.out.println("Error creating table: " + e.getMessage());
+
+        }
+    }
+
+    // Trigger creation of the tables
+    public static void main(String[] args) {
+        createTables();
+    }
 }
