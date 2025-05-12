3f3a232e00e46203a52a5d8097904dadd7d1aee1
mirtanov <156001991+mirtanov@users.noreply.github.com>
2025-05-12 10:34:32 +0200

    naar kamer verplaatsen

--- src/main/java/rooms/SprintRetrospective.java
+++ src/main/java/rooms/SprintRetrospective.java
@@ -4,34 +4,67 @@ import classes.IRoom;
 import classes.Monster; //Stagnator
 import classes.Room;
 
+import java.util.Scanner;
+
 public class SprintRetrospective extends Room implements IRoom {
+    private final Scanner scanner = new Scanner(System.in);
+    private boolean isCorrect;
+
     public SprintRetrospective(String question, String object, String name, Monster monster) {
         super(question, object, name, monster);
     }
 
     @Override
     public void introductionText() {
-
+        System.out.println ("Welcome to the SprintRetrospective room!");
+        System.out.println("In this room, you will reflect on how a Scrum Team can learn from their experiences.");
+        System.out.println ("Use your knowledge to identify what can be improved by the Scrum Team.");
     }
 
     @Override
     public void roomTask() {
-
+        System.out.println("Scenario:");
+        System.out.println("The Scrum Team showed the results of their work after several sprints,");
+        System.out.println("but the stakeholders were not happy with the results,");
+        System.out.println("Question:");
+        System.out.println("What can the Scrum Team learn from this experience?");
+        System.out.println("A) The team should work harder");
+        System.out.println("B) The team should regularly involve the stakeholders during Sprint Reviews.");
+        System.out.println("C) That stakeholders don’t understand the technical work anyway, so it’s fine to exclude them.");
+        System.out.println("D) That the Sprint Review is optional and can be skipped if the team is busy.");
+        System.out.println("Type the letter of your answer:");
     }
 
     @Override
     public void roomCheckAnswer() {
-
+        String answer = scanner.nextLine();
+        if (answer.equalsIgnoreCase("B")) {
+            isCorrect = true;
+        } else {
+            isCorrect = false;
+        }
     }
 
     @Override
     public void roomResult() {
+        if (isCorrect) {
+            System.out.println("Correct! You've shown the ability to reflect and improve.");
+            System.out.println("Now go to the last room where you will face HELL!!!.");
+        } else {
+            System.out.println("Incorrect. A strange fog fills the room...");
+            System.out.println("A Retrospective Monster appears!");
+        }
 
     }
 
     @Override
     public void roomFeedback() {
-
+        if (!isCorrect) {
+            System.out.println("The correct answer was B: Better communication with the stakeholders is essential.");
+            System.out.println("The purpose of the scrum team is to get the results the stakeholders wish for, that is why there involvements is important.");
+            System.out.println("In a Sprint Retrospective, the team should reflect on collaboration, tools, and processes.");
+            System.out.println("This helps ensure that future sprints are more effective.");
+        }
     }
 
     @Override
--- src/main/java/rooms/TIARoom.java
+++ src/main/java/rooms/TIARoom.java
@@ -4,7 +4,11 @@ import classes.IRoom;
 import classes.Monster; //The Scrum Reaper
 import classes.Room;
 
+import java.util.Scanner;
+
 public class TIARoom extends Room implements IRoom {
+    private final Scanner scanner = new Scanner(System.in);
+    private boolean isCorrect;
 
     public TIARoom (String question, String object, String name, Monster monster) {
         super(question, object, name, monster);
@@ -12,26 +16,51 @@ public class TIARoom extends Room implements IRoom {
 
     @Override
     public void introductionText() {
-
+        System.out.println ("Welcome to the TIARoom!");
+        System.out.println ("This is the final challenge you must face before you have succeeded this test of intelligence and wisdom.");
+        System.out.println ("You have walked through every chamber and must now show your full understanding of the SCRUM process.");
+        System.out.println ("Good luck!");
     }
 
     @Override
     public void roomTask() {
-
+        System.out.println ("Answer the following question.");
+        System.out.println ("What does the 'T' in TIA stand for?");
+        System.out.println ("Type your answer:");
     }
 
     @Override
     public void roomCheckAnswer() {
-
+        String answer = scanner.nextLine();
+        if (answer.equalsIgnoreCase("Transparency")) {
+            isCorrect = true;
+        }
+        else {
+            isCorrect = false;
+            //System.out.println ("Wrong answer, the 'T' in TIA stands for 'Transparency'.");
+        }
     }
 
     @Override
     public void roomResult() {
-
+        if (isCorrect) {
+            System.out.println ("You have completed the TIARoom challenge and the SCRUM game!");
+        }
+        else {
+            System.out.println ("You have failed!");
+            // Summon monster
+            System.out.println ("The BOSS monster has appeared in the TIARoom!!!");
+        }
     }
     @Override
     public void roomFeedback() {
-
+        if (!isCorrect) {
+            System.out.println("The correct answer was: Transparency.");
+            System.out.println("In Scrum, Transparency is one of the three pillars.");
+            System.out.println("Transparency means that all people involved in the SCRUM process are transparent with each other.");
+            System.out.println("They all trust each other, and give each other the good news as well as the bad news.");
+            System.out.println("Everyone collaborates for the common purpose that they have decided to achieve");
+        }
     }
     @Override
     public void multipleChoiceQuestion() {
